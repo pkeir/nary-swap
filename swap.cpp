af2b8d5385e80ed06ee51713fa7612173c0e8966
Paul Keir <paul.keir@uws.ac.uk>
2018-11-03 11:07:54 +0000

    A version of std::swap which works for 1 or more variables using C++17 fold expressions.

---
+++ swap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "swap.hpp"
+
+int main(int argc, char *argv[])
+{
+  using namespace nary;
+
+  double d{3.14};
+  swap(d);
+  std::cout << d << '\n';
+
+  int x = 7, y = 8;
+  swap(x,y);
+  std::cout << x << ',' << y << '\n'; // 8,7
+  swap(x,y);  
+  std::cout << x << ',' << y << '\n'; // 8,7
+
+  int a1 = 1, a2 = 2, a3 = 3;
+  std::cout << a1 << ',' << a2 << ',' << a3 << '\n'; // 1,2,3
+  swap(a1,a2,a3);
+  std::cout << a1 << ',' << a2 << ',' << a3 << '\n'; // 2,3,1
+  swap(a1,a2,a3);
+  std::cout << a1 << ',' << a2 << ',' << a3 << '\n'; // 3,1,2
+  swap(a1,a2,a3);
+  std::cout << a1 << ',' << a2 << ',' << a3 << '\n'; // 1,2,3
+
+  return 0;
+}
