bb9f3f59368b19a3ccfdd34dfcb7399ef94e01c5
Paul Keir <paul.keir@uws.ac.uk>
2018-11-03 14:17:40 +0000

    Added constexpr support and tests of that.

--- swap.cpp
+++ swap.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
+#include <tuple>
 #include "swap.hpp"
 
 template <typename T>
-constexpr bool test3() 
+constexpr bool test_3()
 {
-  using namespace nary;
-  T a1 = 1, a2 = 2, a3 = 3;
-  swap(a1,a2,a3);
-  swap(a1,a2,a3);
-  swap(a1,a2,a3);
+  T a1{1}, a2{2}, a3{3};
+  nary::swap(a1,a2,a3);
+  nary::swap(a1,a2,a3);
+  nary::swap(a1,a2,a3);
   return a1 == 1 && a2 == 2 && a3 == 3;
+}
+
+template <std::size_t N, typename ...Ts>
+constexpr bool test_n(Ts &...xs)
+{
+  if constexpr (N==sizeof...(xs)) {
+    std::tuple before(xs...);
+    for (int i = 0; i < N; i++) { nary::swap(xs...); }
+    std::tuple  after(xs...);
+    return before==after;
+  }
+  else {
+    std::size_t x{sizeof...(xs)};
+    return test_n<N>(xs...,x);
+  }
+}
+
+template <auto I, auto ...Is>
+constexpr bool run_tests(std::index_sequence<I,Is...>) {
+  return (... && test_n<Is>());
 }
 
 int main(int argc, char *argv[])
 {
   using namespace nary;
-  static_assert(test3<int>());
+  static_assert(test_3<int>());
+  static_assert(run_tests(std::make_index_sequence<10>{}));
 
   double d{3.14};
   swap(d);
@@ -25,7 +46,7 @@ int main(int argc, char *argv[])
   swap(x,y);
   std::cout << x << ',' << y << '\n'; // 8,7
   swap(x,y);  
-  std::cout << x << ',' << y << '\n'; // 8,7
+  std::cout << x << ',' << y << '\n'; // 7,8
 
   int a1 = 1, a2 = 2, a3 = 3;
   std::cout << a1 << ',' << a2 << ',' << a3 << '\n'; // 1,2,3
