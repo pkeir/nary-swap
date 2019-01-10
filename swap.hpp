b754bc88949a0e3c7b9e717ac4ce58f2aecbb47d
Paul Keir <pkeir@users.noreply.github.com>
2019-01-10 18:41:15 +0000

    A version of swap which rotates right - using a right fold.

--- swap.hpp
+++ swap.hpp
@@ -21,11 +21,31 @@ noexcept (
 {
   T tmp = std::move(x);
   struct wrap {
-    constexpr wrap operator+(wrap &&w) { x = std::move(w.x); return w; }
+    constexpr wrap operator+(wrap &&w) { x = std::move(w.x); return     w; }
     T &x;
   };
   auto c = [](auto &...xs) { (... + wrap{xs}); };
-  c(x,xs...,tmp);
+  c(tmp,x,xs...,tmp);
+}
+
+constexpr void swapr() {}
+
+template <typename T, typename ...Ts>
+constexpr
+std::enable_if_t<std::conjunction_v<std::is_same<T,Ts>...>>
+swapr(T &x, Ts &...xs)
+noexcept (
+  std::is_nothrow_move_constructible_v<T> &&
+  std::is_nothrow_move_assignable_v<T>
+)
+{
+  T tmp = std::move(x);
+  struct wrap {
+    constexpr wrap operator+(wrap &&w) { w.x = std::move(x); return *this; }
+    T &x;
+  };
+  auto c = [](auto &...xs) { (wrap{xs} + ...); };
+  c(tmp,x,xs...,tmp);
 }
 
 } // namespace nary
@@ -38,5 +58,12 @@ constexpr void swap(T &t1, T &t2)
   t1 = std::move(t2);
   t2 = std::move(temp);
 }
-*/
 
+template <typename T>
+constexpr void swapr(T &t1, T &t2)
+{
+  T temp = std::move(t2);
+  t2 = std::move(t1);
+  t1 = std::move(temp);
+}
+*/
