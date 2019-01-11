47f489ef8bf386c69ad759ea3a990c5567baf5a2
Paul Keir <paul.keir@uws.ac.uk>
2019-01-11 12:50:35 +0000

    A right-rotate version using a right-fold-expression now working, with tests; though a warning appears due to the many expected non-usages of each comma operator's left argument.

--- swap.cpp
+++ swap.cpp
@@ -15,11 +15,11 @@ constexpr bool test_3()
   nary::swap(a1,a2,a3);
   nary::swap(a1,a2,a3);
   bool left  = (a1 == 1 && a2 == 2 && a3 == 3);
-//  nary::swapr(a1,a2,a3);
-//  nary::swapr(a1,a2,a3);
-//  nary::swapr(a1,a2,a3);
-//  bool right = (a1 == 1 && a2 == 2 && a3 == 3);
-  return left;// && right;
+  nary::swapr(a1,a2,a3);
+  nary::swapr(a1,a2,a3);
+  nary::swapr(a1,a2,a3);
+  bool right = (a1 == 1 && a2 == 2 && a3 == 3);
+  return left && right;
 }
 
 template <std::size_t N, typename ...Ts>
@@ -30,10 +30,10 @@ constexpr bool test_n(Ts &...xs)
     for (std::size_t i = 0; i < N; i++) { nary::swap(xs...); }
     std::tuple after  = std::make_tuple(xs...);
     bool left  = (before==after);
-//    for (std::size_t i = 0; i < N; i++) { nary::swapr(xs...); }
-//    std::tuple afterr = std::make_tuple(xs...);
-//    bool right = (before==afterr);
-    return left;// && right;
+    for (std::size_t i = 0; i < N; i++) { nary::swapr(xs...); }
+    std::tuple afterr = std::make_tuple(xs...);
+    bool right = (before==afterr);
+    return left && right;
   }
   else {
     std::size_t x{sizeof...(xs)};
@@ -65,12 +65,16 @@ int main(int argc, char *argv[])
         std::vector<int> v1(v1_orig),  v2(v2_orig),    v3(v3_orig);
   swap(v1,v2,v3);
   assert(v1==v2_orig && v2==v3_orig && v3==v1_orig);
+  swapr(v1,v2,v3);
+  assert(v1==v1_orig && v2==v2_orig && v3==v3_orig);
 
         auto p1      = std::make_unique<int>(42);
         auto p2      = std::make_unique<int>(43);
         auto p3      = std::make_unique<int>(44);
   swap(p1,p2,p3);
   assert(*p1==43 && *p2==44 && *p3==42);
+  swapr(p1,p2,p3);
+  assert(*p1==42 && *p2==43 && *p3==44);
 
   int a1{1}, a2{2}, a3{3};
   std::cout << a1 << ',' << a2 << ',' << a3 << '\n'; // 1,2,3
